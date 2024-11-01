a18612dd9c509ca4f57a88d274ca2afed9a76465
Kirsten <kirstensutantyo@gmail.com>
2024-11-01 12:00:09 -0400

    added title and hover

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <nav className='sticky inset-x-0 top-0 z-30 w-full shadow-lg shadow-cyan-500/80 bg-white/90 backdrop-blur-lg transition-all'>
             <MaxWidthWrapper>
                 <div className='flex h-full items-center justify-between py-6'>
-                    <Link href={"/"}><Image src={Logo} alt='UTES logo' height={80} width={80} quality={100} className='duration-300 hover:scale-105 active:scale-95'/></Link>
+                    <Link href={"/"} className='flex flex-row duration-300 hover:scale-105 active:scale-95'><Image src={Logo} alt='UTES logo' height={80} width={80} quality={100}/> <h1 className='w-60 flex-wrap pt-4 pl-5 text-primary'>University of Toronto Esports</h1> </Link>
                     {/* non mobile nav */}
                     <div className='hidden md:flex lg:space-x-10 space-x-6 font-bold lg:text-2xl text-lg uppercase text-primary tracking-widest'>
                         <Link href={"/about"} className='duration-300 hover:scale-105 active:scale-95 hover:text-cyan-500'>
